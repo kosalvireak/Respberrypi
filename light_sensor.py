9602dcf643a045cd34b3dcc1eb270fbfc3832a50
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-26 15:43:55 +0700

    add source code

---
+++ light_sensor.py
@@ -0,0 +1,8 @@
+# https://www.uugear.com/portfolio/using-light-sensor-module-with-raspberry-pi/
+
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+for i in range(0,5):
+    print GPIO.input(4)
